d590b9eb7f8397da184a2e3da143a55b5a2d5ec7
D. Port Me <dport@hawaii.edu>
2021-10-22 19:16:18 -1000

    xxx

--- morea/130.Assignment1/SmartPhoneProductsA1_Example/product_data.js
+++ morea/130.Assignment1/SmartPhoneProductsA1_Example/product_data.js
@@ -4,27 +4,32 @@ var products =
     {
     "brand": "HTC",
     "price": 40.00,
-    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+    "quantity_avail": 2
     },
     {
     "brand": "Apple",
     "price": 75.00,
-    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+    "quantity_avail": 6
     },
     {
     "brand": "Nokia",
     "price": 35.00,
-    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+    "quantity_avail": 20
     },
     {
     "brand": "Samsung",
     "price": 45.00,
-    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+    "quantity_avail": 3
     },
     {
     "brand": "Blackberry",
     "price": 10.00,
-    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+    "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+    "quantity_avail": 15
     }
 ];
 if(typeof module != 'undefined') {
